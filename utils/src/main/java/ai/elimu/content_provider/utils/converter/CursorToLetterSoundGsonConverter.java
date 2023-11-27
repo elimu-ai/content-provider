82f4ca62e1a02193f7f64f3245f6043f79b69754
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2023-11-27 16:09:27 +0700

    refactor: Remove wrong columns

--- utils/src/main/java/ai/elimu/content_provider/utils/converter/CursorToLetterSoundGsonConverter.java
+++ utils/src/main/java/ai/elimu/content_provider/utils/converter/CursorToLetterSoundGsonConverter.java
@@ -22,14 +22,6 @@ public class CursorToLetterSoundGsonConverter {
         Integer revisionNumber = cursor.getInt(columnIndexRevisionNumber);
         Log.i(CursorToLetterSoundGsonConverter.class.getName(), "revisionNumber: " + revisionNumber);
 
-        int columnIndexText = cursor.getColumnIndex("text");
-        String text = cursor.getString(columnIndexText);
-        Log.i(CursorToLetterSoundGsonConverter.class.getName(), "text: \"" + text + "\"");
-
-        int columnIndexIsDiacritic = cursor.getColumnIndex("diacritic");
-        Boolean isDiacritic = (cursor.getInt(columnIndexIsDiacritic) > 0);
-        Log.i(CursorToLetterSoundGsonConverter.class.getName(), "isDiacritic: " + isDiacritic);
-
         LetterSoundGson letterSound = new LetterSoundGson();
         letterSound.setId(id);
         letterSound.setRevisionNumber(revisionNumber);
