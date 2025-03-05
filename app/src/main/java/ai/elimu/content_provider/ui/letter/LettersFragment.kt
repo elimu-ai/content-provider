bef8a71f04d60f05f683d5efa19fb6c28ecbbea1
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-05 14:32:30 +0700

    Avoid repeatedly extracting log tag from class name in runtime

--- app/src/main/java/ai/elimu/content_provider/ui/letter/LettersFragment.kt
+++ app/src/main/java/ai/elimu/content_provider/ui/letter/LettersFragment.kt
@@ -29,12 +29,14 @@ class LettersFragment : Fragment() {
 
     private var textView: TextView? = null
 
+    private val TAG = javaClass.name
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.i(javaClass.name, "onCreateView")
+        Log.i(TAG, "onCreateView")
 
         lettersViewModel = ViewModelProvider(this).get(
             LettersViewModel::class.java
@@ -44,7 +46,7 @@ class LettersFragment : Fragment() {
         textView = root.findViewById(R.id.text_letters) as? TextView
         lettersViewModel!!.text.observe(viewLifecycleOwner, object : Observer<String?> {
             override fun onChanged(s: String?) {
-                Log.i(javaClass.name, "onChanged")
+                Log.i(TAG, "onChanged")
                 textView?.text = s
             }
         })
@@ -52,7 +54,7 @@ class LettersFragment : Fragment() {
     }
 
     override fun onStart() {
-        Log.i(javaClass.name, "onStart")
+        Log.i(TAG, "onStart")
         super.onStart()
 
         // Download Letters from REST API, and store them in the database
@@ -60,18 +62,18 @@ class LettersFragment : Fragment() {
         val retrofit = baseApplication.retrofit
         val lettersService = retrofit.create(LettersService::class.java)
         val letterGsonsCall = lettersService.listLetters()
-        Log.i(javaClass.name, "letterGsonsCall.request(): " + letterGsonsCall.request())
+        Log.i(TAG, "letterGsonsCall.request(): " + letterGsonsCall.request())
         letterGsonsCall.enqueue(object : Callback<List<LetterGson>> {
             override fun onResponse(
                 call: Call<List<LetterGson>>,
                 response: Response<List<LetterGson>>
             ) {
-                Log.i(javaClass.name, "onResponse")
+                Log.i(TAG, "onResponse")
 
-                Log.i(javaClass.name, "response: $response")
+                Log.i(TAG, "response: $response")
                 if (response.isSuccessful) {
                     val letterGsons = response.body()!!
-                    Log.i(javaClass.name, "letterGsons.size(): " + letterGsons.size)
+                    Log.i(TAG, "letterGsons.size(): " + letterGsons.size)
 
                     if (letterGsons.size > 0) {
                         processResponseBody(letterGsons)
@@ -86,9 +88,9 @@ class LettersFragment : Fragment() {
             }
 
             override fun onFailure(call: Call<List<LetterGson>>, t: Throwable) {
-                Log.e(javaClass.name, "onFailure", t)
+                Log.e(TAG, "onFailure", t)
 
-                Log.e(javaClass.name, "t.getCause():", t.cause)
+                Log.e(TAG, "t.getCause():", t.cause)
 
                 // Handle error
                 Snackbar.make(textView!!, t.cause.toString(), Snackbar.LENGTH_LONG)
@@ -100,12 +102,12 @@ class LettersFragment : Fragment() {
     }
 
     private fun processResponseBody(letterGsons: List<LetterGson>) {
-        Log.i(javaClass.name, "processResponseBody")
+        Log.i(TAG, "processResponseBody")
 
         val executorService = Executors.newSingleThreadExecutor()
         executorService.execute(object : Runnable {
             override fun run() {
-                Log.i(javaClass.name, "run")
+                Log.i(TAG, "run")
 
                 val roomDb = RoomDb.getDatabase(context)
                 val letterDao = roomDb.letterDao()
@@ -114,17 +116,17 @@ class LettersFragment : Fragment() {
                 letterDao.deleteAll()
 
                 for (letterGson in letterGsons) {
-                    Log.i(javaClass.name, "letterGson.getId(): " + letterGson.id)
+                    Log.i(TAG, "letterGson.getId(): " + letterGson.id)
 
                     // Store the Letter in the database
                     val letter = GsonToRoomConverter.getLetter(letterGson)
                     letterDao.insert(letter)
-                    Log.i(javaClass.name, "Stored Letter in database with ID " + letter.id)
+                    Log.i(TAG, "Stored Letter in database with ID " + letter.id)
                 }
 
                 // Update the UI
                 val letters = letterDao.loadAllOrderedByUsageCount()
-                Log.i(javaClass.name, "letters.size(): " + letters.size)
+                Log.i(TAG, "letters.size(): " + letters.size)
                 activity!!.runOnUiThread {
                     textView!!.text = "letters.size(): " + letters.size
                     Snackbar.make(
