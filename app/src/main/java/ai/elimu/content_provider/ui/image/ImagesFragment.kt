ca4786acbcfaf0a4b1128c32ce5d5a948a6d1b27
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-05 14:18:49 +0700

    Avoid repeatedly extracting log tag from class name in runtime

--- app/src/main/java/ai/elimu/content_provider/ui/image/ImagesFragment.kt
+++ app/src/main/java/ai/elimu/content_provider/ui/image/ImagesFragment.kt
@@ -35,12 +35,14 @@ class ImagesFragment : Fragment() {
 
     private var textView: TextView? = null
 
+    private val TAG = ImagesFragment::class.java.name
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.i(javaClass.name, "onCreateView")
+        Log.i(TAG, "onCreateView")
 
         imagesViewModel = ViewModelProvider(this).get(ImagesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_images, container, false)
@@ -48,7 +50,7 @@ class ImagesFragment : Fragment() {
         textView = root.findViewById(R.id.text_images) as? TextView
         imagesViewModel!!.text.observe(viewLifecycleOwner, object : Observer<String?> {
             override fun onChanged(s: String?) {
-                Log.i(javaClass.name, "onChanged")
+                Log.i(TAG, "onChanged")
                 textView?.text = s
             }
         })
@@ -56,7 +58,7 @@ class ImagesFragment : Fragment() {
     }
 
     override fun onStart() {
-        Log.i(javaClass.name, "onStart")
+        Log.i(TAG, "onStart")
         super.onStart()
 
         // Download Images from REST API, and store them in the database
@@ -64,18 +66,18 @@ class ImagesFragment : Fragment() {
         val retrofit = baseApplication.retrofit
         val imagesService = retrofit.create(ImagesService::class.java)
         val imageGsonsCall = imagesService.listImages()
-        Log.i(javaClass.name, "imageGsonsCall.request(): " + imageGsonsCall.request())
+        Log.i(TAG, "imageGsonsCall.request(): " + imageGsonsCall.request())
         imageGsonsCall.enqueue(object : Callback<List<ImageGson>> {
             override fun onResponse(
                 call: Call<List<ImageGson>>,
                 response: Response<List<ImageGson>>
             ) {
-                Log.i(javaClass.name, "onResponse")
+                Log.i(TAG, "onResponse")
 
-                Log.i(javaClass.name, "response: $response")
+                Log.i(TAG, "response: $response")
                 if (response.isSuccessful) {
                     val imageGsons = response.body()!!
-                    Log.i(javaClass.name, "imageGsons.size(): " + imageGsons.size)
+                    Log.i(TAG, "imageGsons.size(): " + imageGsons.size)
 
                     if (imageGsons.size > 0) {
                         processResponseBody(imageGsons)
@@ -90,9 +92,9 @@ class ImagesFragment : Fragment() {
             }
 
             override fun onFailure(call: Call<List<ImageGson>>, t: Throwable) {
-                Log.e(javaClass.name, "onFailure", t)
+                Log.e(TAG, "onFailure", t)
 
-                Log.e(javaClass.name, "t.getCause():", t.cause)
+                Log.e(TAG, "t.getCause():", t.cause)
 
                 // Handle error
                 Snackbar.make(textView!!, t.cause.toString(), Snackbar.LENGTH_LONG)
@@ -104,12 +106,12 @@ class ImagesFragment : Fragment() {
     }
 
     private fun processResponseBody(imageGsons: List<ImageGson>) {
-        Log.i(javaClass.name, "processResponseBody")
+        Log.i(TAG, "processResponseBody")
 
         val executorService = Executors.newSingleThreadExecutor()
         executorService.execute(object : Runnable {
             override fun run() {
-                Log.i(javaClass.name, "run")
+                Log.i(TAG, "run")
 
                 val roomDb = RoomDb.getDatabase(context)
                 val imageDao = roomDb.imageDao()
@@ -121,14 +123,14 @@ class ImagesFragment : Fragment() {
 
                 // TODO: also delete corresponding image files (only those that are no longer used)
                 for (imageGson in imageGsons) {
-                    Log.i(javaClass.name, "imageGson.getId(): " + imageGson.id)
+                    Log.i(TAG, "imageGson.getId(): " + imageGson.id)
 
                     val image = GsonToRoomConverter.getImage(imageGson)
 
                     // Check if the corresponding image file has already been downloaded
                     val imageFile = FileHelper.getImageFile(imageGson, context)
-                    Log.i(javaClass.name, "imageFile: $imageFile")
-                    Log.i(javaClass.name, "imageFile.exists(): " + imageFile.exists())
+                    Log.i(TAG, "imageFile: $imageFile")
+                    Log.i(TAG, "imageFile.exists(): " + imageFile.exists())
                     if (!imageFile.exists()) {
                         // Download file bytes
                         val baseApplication = activity!!.application as BaseApplication
@@ -136,37 +138,37 @@ class ImagesFragment : Fragment() {
                             imageGson.bytesUrl
                         else
                             baseApplication.baseUrl + imageGson.bytesUrl
-                        Log.i(javaClass.name, "downloadUrl: $downloadUrl")
+                        Log.i(TAG, "downloadUrl: $downloadUrl")
                         val bytes = MultimediaDownloader.downloadFileBytes(downloadUrl)
-                        Log.i(javaClass.name, "bytes.length: " + bytes.size)
+                        Log.i(TAG, "bytes.length: " + bytes.size)
 
                         // Store the downloaded file in the external storage directory
                         try {
                             val fileOutputStream = FileOutputStream(imageFile)
                             fileOutputStream.write(bytes)
                         } catch (e: FileNotFoundException) {
-                            Log.e(javaClass.name, null, e)
+                            Log.e(TAG, null, e)
                         } catch (e: IOException) {
-                            Log.e(javaClass.name, null, e)
+                            Log.e(TAG, null, e)
                         }
-                        Log.i(javaClass.name, "imageFile.exists(): " + imageFile.exists())
+                        Log.i(TAG, "imageFile.exists(): " + imageFile.exists())
                     }
 
                     // Store the Image in the database
                     imageDao.insert(image)
-                    Log.i(javaClass.name, "Stored Image in database with ID " + image.id)
+                    Log.i(TAG, "Stored Image in database with ID " + image.id)
 
                     // Store all the Image's Word labels in the database
                     val wordGsons = imageGson.words
-                    Log.i(javaClass.name, "wordGsons.size(): " + wordGsons.size)
+                    Log.i(TAG, "wordGsons.size(): " + wordGsons.size)
                     for (wordGson in wordGsons) {
-                        Log.i(javaClass.name, "wordGson.getId(): " + wordGson.id)
+                        Log.i(TAG, "wordGson.getId(): " + wordGson.id)
                         val image_Word = Image_Word()
                         image_Word.image_id = imageGson.id
                         image_Word.words_id = wordGson.id
                         image_WordDao.insert(image_Word)
                         Log.i(
-                            javaClass.name,
+                            TAG,
                             "Stored Image_Word in database. Image_id: " + image_Word.image_id + ", words_id: " + image_Word.words_id
                         )
                     }
@@ -174,7 +176,7 @@ class ImagesFragment : Fragment() {
 
                 // Update the UI
                 val images = imageDao.loadAll()
-                Log.i(javaClass.name, "images.size(): " + images.size)
+                Log.i(TAG, "images.size(): " + images.size)
                 activity!!.runOnUiThread {
                     textView!!.text = "images.size(): " + images.size
                     Snackbar.make(textView!!, "images.size(): " + images.size, Snackbar.LENGTH_LONG)
