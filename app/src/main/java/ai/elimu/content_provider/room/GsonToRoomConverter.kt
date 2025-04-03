5d0f07caf58c1a2178fa596df91a77612a9fd9fe
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-03 10:27:44 +0700

    Convert GsonToRoomConverter to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/GsonToRoomConverter.kt
+++ app/src/main/java/ai/elimu/content_provider/room/GsonToRoomConverter.kt
@@ -1,260 +1,266 @@
-package ai.elimu.content_provider.room;
+package ai.elimu.content_provider.room
 
-import ai.elimu.content_provider.room.entity.LetterSound;
-import ai.elimu.content_provider.room.entity.Sound;
-import ai.elimu.content_provider.room.entity.Emoji;
-import ai.elimu.content_provider.room.entity.Image;
-import ai.elimu.content_provider.room.entity.Letter;
-import ai.elimu.content_provider.room.entity.Number;
-import ai.elimu.content_provider.room.entity.StoryBook;
-import ai.elimu.content_provider.room.entity.StoryBookChapter;
-import ai.elimu.content_provider.room.entity.StoryBookParagraph;
-import ai.elimu.content_provider.room.entity.Video;
-import ai.elimu.content_provider.room.entity.Word;
-import ai.elimu.model.v2.gson.content.LetterSoundGson;
-import ai.elimu.model.v2.gson.content.SoundGson;
-import ai.elimu.model.v2.gson.content.EmojiGson;
-import ai.elimu.model.v2.gson.content.ImageGson;
-import ai.elimu.model.v2.gson.content.LetterGson;
-import ai.elimu.model.v2.gson.content.NumberGson;
-import ai.elimu.model.v2.gson.content.StoryBookChapterGson;
-import ai.elimu.model.v2.gson.content.StoryBookGson;
-import ai.elimu.model.v2.gson.content.StoryBookParagraphGson;
-import ai.elimu.model.v2.gson.content.VideoGson;
-import ai.elimu.model.v2.gson.content.WordGson;
+import ai.elimu.content_provider.room.entity.Emoji
+import ai.elimu.content_provider.room.entity.Image
+import ai.elimu.content_provider.room.entity.Letter
+import ai.elimu.content_provider.room.entity.LetterSound
+import ai.elimu.content_provider.room.entity.Number
+import ai.elimu.content_provider.room.entity.Sound
+import ai.elimu.content_provider.room.entity.StoryBook
+import ai.elimu.content_provider.room.entity.StoryBookChapter
+import ai.elimu.content_provider.room.entity.StoryBookParagraph
+import ai.elimu.content_provider.room.entity.Video
+import ai.elimu.content_provider.room.entity.Word
+import ai.elimu.model.v2.gson.content.EmojiGson
+import ai.elimu.model.v2.gson.content.ImageGson
+import ai.elimu.model.v2.gson.content.LetterGson
+import ai.elimu.model.v2.gson.content.LetterSoundGson
+import ai.elimu.model.v2.gson.content.NumberGson
+import ai.elimu.model.v2.gson.content.SoundGson
+import ai.elimu.model.v2.gson.content.StoryBookChapterGson
+import ai.elimu.model.v2.gson.content.StoryBookGson
+import ai.elimu.model.v2.gson.content.StoryBookParagraphGson
+import ai.elimu.model.v2.gson.content.VideoGson
+import ai.elimu.model.v2.gson.content.WordGson
 
-public class GsonToRoomConverter {
-
-    public static Letter getLetter(LetterGson letterGson) {
+object GsonToRoomConverter {
+    fun getLetter(letterGson: LetterGson?): Letter? {
         if (letterGson == null) {
-            return null;
+            return null
         } else {
-            Letter letter = new Letter();
+            val letter = Letter()
 
             // BaseEntity
-            letter.setId(letterGson.getId());
+            letter.id = letterGson.id
 
             // Content
-            letter.setRevisionNumber(letterGson.getRevisionNumber());
-            letter.setUsageCount(letterGson.getUsageCount());
+            letter.revisionNumber = letterGson.revisionNumber
+            letter.usageCount = letterGson.usageCount
 
             // Letter
-            letter.setText(letterGson.getText());
-            letter.setDiacritic(letterGson.getDiacritic());
+            letter.text = letterGson.text
+            letter.isDiacritic = letterGson.diacritic
 
-            return letter;
+            return letter
         }
     }
 
-    public static Sound getSound(SoundGson soundGson) {
+    @JvmStatic
+    fun getSound(soundGson: SoundGson?): Sound? {
         if (soundGson == null) {
-            return null;
+            return null
         } else {
-            Sound sound = new Sound();
+            val sound = Sound()
 
             // BaseEntity
-            sound.setId(soundGson.getId());
+            sound.id = soundGson.id
 
             // Content
-            sound.setRevisionNumber(soundGson.getRevisionNumber());
-            sound.setUsageCount(soundGson.getUsageCount());
+            sound.revisionNumber = soundGson.revisionNumber
+            sound.usageCount = soundGson.usageCount
 
             // Sound
-            sound.setValueIpa(soundGson.getValueIpa());
-            sound.setDiacritic(soundGson.getDiacritic());
+            sound.valueIpa = soundGson.valueIpa
+            sound.diacritic = soundGson.diacritic
 
-            return sound;
+            return sound
         }
     }
 
-    public static LetterSound getLetterSound(LetterSoundGson letterSoundGson) {
+    fun getLetterSound(letterSoundGson: LetterSoundGson?): LetterSound? {
         if (letterSoundGson == null) {
-            return null;
+            return null
         } else {
-            LetterSound letterSound = new LetterSound();
+            val letterSound = LetterSound()
 
             // BaseEntity
-            letterSound.setId(letterSoundGson.getId());
+            letterSound.id = letterSoundGson.id
 
             // Content
-            letterSound.setRevisionNumber(letterSoundGson.getRevisionNumber());
-            letterSound.setUsageCount(letterSoundGson.getUsageCount());
+            letterSound.revisionNumber = letterSoundGson.revisionNumber
+            letterSound.usageCount = letterSoundGson.usageCount
 
             // Letter-sound correspondence
             // Note: letters are stored separately in LetterSound_Letter (see LetterSoundsFragment.java)
             // Note: sounds are stored separately in LetterSound_Sound (see LetterSoundsFragment.java)
-
-            return letterSound;
+            return letterSound
         }
     }
 
-    public static Word getWord(WordGson wordGson) {
+    @JvmStatic
+    fun getWord(wordGson: WordGson?): Word? {
         if (wordGson == null) {
-            return null;
+            return null
         } else {
-            Word word = new Word();
+            val word = Word()
 
             // BaseEntity
-            word.setId(wordGson.getId());
+            word.id = wordGson.id
 
             // Content
-            word.setRevisionNumber(wordGson.getRevisionNumber());
-            word.setUsageCount(wordGson.getUsageCount());
+            word.revisionNumber = wordGson.revisionNumber
+            word.usageCount = wordGson.usageCount
 
             // Word
-            word.setText(wordGson.getText());
-            word.setWordType(wordGson.getWordType());
+            word.text = wordGson.text
+            word.wordType = wordGson.wordType
 
-            return word;
+            return word
         }
     }
 
-    public static Number getNumber(NumberGson numberGson) {
+    @JvmStatic
+    fun getNumber(numberGson: NumberGson?): Number? {
         if (numberGson == null) {
-            return null;
+            return null
         } else {
-            Number number = new Number();
+            val number = Number()
 
             // BaseEntity
-            number.setId(numberGson.getId());
+            number.id = numberGson.id
 
             // Content
-            number.setRevisionNumber(numberGson.getRevisionNumber());
-            number.setUsageCount(numberGson.getUsageCount());
+            number.revisionNumber = numberGson.revisionNumber
+            number.usageCount = numberGson.usageCount
 
             // Number
-            number.setValue(numberGson.getValue());
-            number.setSymbol(numberGson.getSymbol());
+            number.value = numberGson.value
+            number.symbol = numberGson.symbol
 
-            return number;
+            return number
         }
     }
 
-    public static Emoji getEmoji(EmojiGson emojiGson) {
+    @JvmStatic
+    fun getEmoji(emojiGson: EmojiGson?): Emoji? {
         if (emojiGson == null) {
-            return null;
+            return null
         } else {
-            Emoji emoji = new Emoji();
+            val emoji = Emoji()
 
             // BaseEntity
-            emoji.setId(emojiGson.getId());
+            emoji.id = emojiGson.id
 
             // Content
-            emoji.setRevisionNumber(emojiGson.getRevisionNumber());
-            emoji.setUsageCount(emojiGson.getUsageCount());
+            emoji.revisionNumber = emojiGson.revisionNumber
+            emoji.usageCount = emojiGson.usageCount
 
             // Emoji
-            emoji.setGlyph(emojiGson.getGlyph());
-            emoji.setUnicodeVersion(emojiGson.getUnicodeVersion());
-            emoji.setUnicodeEmojiVersion(emojiGson.getUnicodeEmojiVersion());
-            // Note: words are stored separately in Emoji_Word (see EmojisFragment.java)
+            emoji.glyph = emojiGson.glyph
+            emoji.unicodeVersion = emojiGson.unicodeVersion
+            emoji.unicodeEmojiVersion = emojiGson.unicodeEmojiVersion
 
-            return emoji;
+            // Note: words are stored separately in Emoji_Word (see EmojisFragment.java)
+            return emoji
         }
     }
 
-    public static Image getImage(ImageGson imageGson) {
+    fun getImage(imageGson: ImageGson?): Image? {
         if (imageGson == null) {
-            return null;
+            return null
         } else {
-            Image image = new Image();
+            val image = Image()
 
             // BaseEntity
-            image.setId(imageGson.getId());
+            image.id = imageGson.id
 
             // Content
-            image.setRevisionNumber(imageGson.getRevisionNumber());
-            image.setUsageCount(imageGson.getUsageCount());
+            image.revisionNumber = imageGson.revisionNumber
+            image.usageCount = imageGson.usageCount
 
             // Image
-            image.setTitle(imageGson.getTitle());
-            image.setImageFormat(imageGson.getImageFormat());
-            // Note: words are stored separately in Image_Word (see ImagesFragment.java)
+            image.title = imageGson.title
+            image.imageFormat = imageGson.imageFormat
 
-            return image;
+            // Note: words are stored separately in Image_Word (see ImagesFragment.java)
+            return image
         }
     }
 
-    public static StoryBook getStoryBook(StoryBookGson storyBookGson) {
+    @JvmStatic
+    fun getStoryBook(storyBookGson: StoryBookGson?): StoryBook? {
         if (storyBookGson == null) {
-            return null;
+            return null
         } else {
-            StoryBook storyBook = new StoryBook();
+            val storyBook = StoryBook()
 
             // BaseEntity
-            storyBook.setId(storyBookGson.getId());
+            storyBook.id = storyBookGson.id
 
             // Content
-            storyBook.setRevisionNumber(storyBookGson.getRevisionNumber());
-            storyBook.setUsageCount(storyBookGson.getUsageCount());
+            storyBook.revisionNumber = storyBookGson.revisionNumber
+            storyBook.usageCount = storyBookGson.usageCount
 
             // StoryBook
-            storyBook.setTitle(storyBookGson.getTitle());
-            storyBook.setDescription(storyBookGson.getDescription());
-            storyBook.setCoverImageId(storyBookGson.getCoverImage().getId());
-            storyBook.setReadingLevel(storyBookGson.getReadingLevel());
+            storyBook.title = storyBookGson.title
+            storyBook.description = storyBookGson.description
+            storyBook.coverImageId = storyBookGson.coverImage.id
+            storyBook.readingLevel = storyBookGson.readingLevel
 
-            return storyBook;
+            return storyBook
         }
     }
 
-    public static StoryBookChapter getStoryBookChapter(StoryBookChapterGson storyBookChapterGson) {
+    @JvmStatic
+    fun getStoryBookChapter(storyBookChapterGson: StoryBookChapterGson?): StoryBookChapter? {
         if (storyBookChapterGson == null) {
-            return null;
+            return null
         } else {
-            StoryBookChapter storyBookChapter = new StoryBookChapter();
+            val storyBookChapter = StoryBookChapter()
 
             // BaseEntity
-            storyBookChapter.setId(storyBookChapterGson.getId());
+            storyBookChapter.id = storyBookChapterGson.id
 
             // StoryBookChapter
 //            storyBookChapter.setStoryBookId(storyBookChapterGson.getStoryBook().getId());
-            storyBookChapter.setSortOrder(storyBookChapterGson.getSortOrder());
-            if (storyBookChapterGson.getImage() != null) {
-                storyBookChapter.setImageId(storyBookChapterGson.getImage().getId());
+            storyBookChapter.sortOrder = storyBookChapterGson.sortOrder
+            if (storyBookChapterGson.image != null) {
+                storyBookChapter.imageId = storyBookChapterGson.image.id
             }
 
-            return storyBookChapter;
+            return storyBookChapter
         }
     }
 
-    public static StoryBookParagraph getStoryBookParagraph(StoryBookParagraphGson storyBookParagraphGson) {
+    @JvmStatic
+    fun getStoryBookParagraph(storyBookParagraphGson: StoryBookParagraphGson?): StoryBookParagraph? {
         if (storyBookParagraphGson == null) {
-            return null;
+            return null
         } else {
-            StoryBookParagraph storyBookParagraph = new StoryBookParagraph();
+            val storyBookParagraph = StoryBookParagraph()
 
             // BaseEntity
-            storyBookParagraph.setId(storyBookParagraphGson.getId());
+            storyBookParagraph.id = storyBookParagraphGson.id
 
             // StoryBookParagraph
 //            storyBookParagraph.setStoryBookChapterId(storyBookParagraphGson.getStoryBookChapter().getId());
-            storyBookParagraph.setSortOrder(storyBookParagraphGson.getSortOrder());
-            storyBookParagraph.setOriginalText(storyBookParagraphGson.getOriginalText());
-            // Note: words are stored separately in StoryBookParagraph_Word (see StoryBooksFragment.java)
+            storyBookParagraph.sortOrder = storyBookParagraphGson.sortOrder
+            storyBookParagraph.originalText = storyBookParagraphGson.originalText
 
-            return storyBookParagraph;
+            // Note: words are stored separately in StoryBookParagraph_Word (see StoryBooksFragment.java)
+            return storyBookParagraph
         }
     }
 
-    public static Video getVideo(VideoGson videoGson) {
+    @JvmStatic
+    fun getVideo(videoGson: VideoGson?): Video? {
         if (videoGson == null) {
-            return null;
+            return null
         } else {
-            Video video = new Video();
+            val video = Video()
 
             // BaseEntity
-            video.setId(videoGson.getId());
+            video.id = videoGson.id
 
             // Content
-            video.setRevisionNumber(videoGson.getRevisionNumber());
-            video.setUsageCount(videoGson.getUsageCount());
+            video.revisionNumber = videoGson.revisionNumber
+            video.usageCount = videoGson.usageCount
 
             // Video
-            video.setTitle(videoGson.getTitle());
-            video.setVideoFormat(videoGson.getVideoFormat());
+            video.title = videoGson.title
+            video.videoFormat = videoGson.videoFormat
 
-            return video;
+            return video
         }
     }
 }
