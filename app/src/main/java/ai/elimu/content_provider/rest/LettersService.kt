a35f8af1e612e61d8edba9d6bf958b28b6058003
tuancoltech <tuancoltech@gmail.com>
2025-04-21 12:51:17 +0700

    Convert LettersService to Kotlin

--- app/src/main/java/ai/elimu/content_provider/rest/LettersService.kt
+++ app/src/main/java/ai/elimu/content_provider/rest/LettersService.kt
@@ -1,13 +1,10 @@
-package ai.elimu.content_provider.rest;
+package ai.elimu.content_provider.rest
 
-import java.util.List;
+import ai.elimu.model.v2.gson.content.LetterGson
+import retrofit2.Call
+import retrofit2.http.GET
 
-import ai.elimu.model.v2.gson.content.LetterGson;
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface LettersService {
-
+interface LettersService {
     @GET("content/letters")
-    Call<List<LetterGson>> listLetters();
+    fun listLetters(): Call<List<LetterGson>>
 }
