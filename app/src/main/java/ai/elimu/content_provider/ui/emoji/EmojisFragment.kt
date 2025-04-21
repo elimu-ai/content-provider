8e4facf14b5c6b1ffb446994255f5dae0faaac93
tuancoltech <tuancoltech@gmail.com>
2025-04-21 08:40:02 +0700

    Convert anonymous classes to lambdas

--- app/src/main/java/ai/elimu/content_provider/ui/emoji/EmojisFragment.kt
+++ app/src/main/java/ai/elimu/content_provider/ui/emoji/EmojisFragment.kt
@@ -15,7 +15,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import retrofit2.Call
@@ -36,14 +35,12 @@ class EmojisFragment : Fragment() {
     ): View {
         Log.i(TAG, "onCreateView")
 
-        emojisViewModel = ViewModelProvider(this).get(EmojisViewModel::class.java)
+        emojisViewModel = ViewModelProvider(this)[EmojisViewModel::class.java]
         binding = FragmentEmojisBinding.inflate(layoutInflater)
-        emojisViewModel?.getText()?.observe(viewLifecycleOwner, object : Observer<String?> {
-            override fun onChanged(s: String?) {
-                Log.i(TAG, "onChanged")
-                binding.textEmojis.text = s
-            }
-        })
+        emojisViewModel?.getText()?.observe(viewLifecycleOwner) { s ->
+            Log.i(TAG, "onChanged")
+            binding.textEmojis.text = s
+        }
         return binding.root
     }
 
@@ -103,54 +100,56 @@ class EmojisFragment : Fragment() {
         Log.i(TAG, "processResponseBody")
 
         val executorService = Executors.newSingleThreadExecutor()
-        executorService.execute(object : Runnable {
-            override fun run() {
-                Log.i(TAG, "run")
+        executorService.execute {
+            Log.i(TAG, "run")
 
-                val roomDb = RoomDb.getDatabase(context)
-                val emojiDao = roomDb.emojiDao()
-                val emojiWordDao = roomDb.emoji_WordDao()
+            val roomDb = RoomDb.getDatabase(context)
+            val emojiDao = roomDb.emojiDao()
+            val emojiWordDao = roomDb.emoji_WordDao()
 
-                // Empty the database table before downloading up-to-date content
-                emojiWordDao.deleteAll()
-                emojiDao.deleteAll()
+            // Empty the database table before downloading up-to-date content
+            emojiWordDao.deleteAll()
+            emojiDao.deleteAll()
 
-                for (emojiGson in emojiGsons) {
-                    Log.i(TAG, "emojiGson.getId(): " + emojiGson.id)
+            for (emojiGson in emojiGsons) {
+                Log.i(TAG, "emojiGson.getId(): " + emojiGson.id)
 
-                    // Store the Emoji in the database
-                    val emoji = getEmoji(emojiGson)
-                    emoji?.let {
-                        emojiDao.insert(emoji)
-                        Log.i(TAG, "Stored Emoji in database with ID " + emoji.id)
-                    }
-
-                    // Store all the Emoji's Word labels in the database
-                    val wordGsons = emojiGson.words
-                    Log.i(TAG, "wordGsons.size(): " + wordGsons.size)
-                    for (wordGson in wordGsons) {
-                        Log.i(TAG, "wordGson.getId(): " + wordGson.id)
-                        val emojiWord = Emoji_Word()
-                        emojiWord.emoji_id = emojiGson.id
-                        emojiWord.words_id = wordGson.id
-                        emojiWordDao.insert(emojiWord)
-                        Log.i(
-                            TAG,
-                            "Stored Emoji_Word in database. Emoji_id: " + emojiWord.emoji_id + ", words_id: " + emojiWord.words_id
-                        )
-                    }
+                // Store the Emoji in the database
+                val emoji = getEmoji(emojiGson)
+                emoji?.let {
+                    emojiDao.insert(emoji)
+                    Log.i(TAG, "Stored Emoji in database with ID " + emoji.id)
                 }
 
-                // Update the UI
-                val emojis = emojiDao.loadAll()
-                Log.i(TAG, "emojis.size(): " + emojis.size)
-                activity?.runOnUiThread {
-                    binding.textEmojis.text = getString(R.string.emojis_size, emojis.size)
-                    Snackbar.make(binding.textEmojis, "emojis.size(): " + emojis.size, Snackbar.LENGTH_LONG)
-                        .show()
-                    binding.progressBarEmojis.visibility = View.GONE
+                // Store all the Emoji's Word labels in the database
+                val wordGsons = emojiGson.words
+                Log.i(TAG, "wordGsons.size(): " + wordGsons.size)
+                for (wordGson in wordGsons) {
+                    Log.i(TAG, "wordGson.getId(): " + wordGson.id)
+                    val emojiWord = Emoji_Word()
+                    emojiWord.emoji_id = emojiGson.id
+                    emojiWord.words_id = wordGson.id
+                    emojiWordDao.insert(emojiWord)
+                    Log.i(
+                        TAG,
+                        "Stored Emoji_Word in database. Emoji_id: " + emojiWord.emoji_id + ", words_id: " + emojiWord.words_id
+                    )
                 }
             }
-        })
+
+            // Update the UI
+            val emojis = emojiDao.loadAll()
+            Log.i(TAG, "emojis.size(): " + emojis.size)
+            activity?.runOnUiThread {
+                binding.textEmojis.text = getString(R.string.emojis_size, emojis.size)
+                Snackbar.make(
+                    binding.textEmojis,
+                    "emojis.size(): " + emojis.size,
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+                binding.progressBarEmojis.visibility = View.GONE
+            }
+        }
     }
 }
