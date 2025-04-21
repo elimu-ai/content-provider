5136882aa6d819e1d63de11f215ad63401b50d1c
tuancoltech <tuancoltech@gmail.com>
2025-04-21 12:59:25 +0700

    Convert ImagesService to Kotlin

--- app/src/main/java/ai/elimu/content_provider/rest/ImagesService.kt
+++ app/src/main/java/ai/elimu/content_provider/rest/ImagesService.kt
@@ -1,13 +1,10 @@
-package ai.elimu.content_provider.rest;
+package ai.elimu.content_provider.rest
 
-import java.util.List;
+import ai.elimu.model.v2.gson.content.ImageGson
+import retrofit2.Call
+import retrofit2.http.GET
 
-import ai.elimu.model.v2.gson.content.ImageGson;
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface ImagesService {
-
+interface ImagesService {
     @GET("content/images")
-    Call<List<ImageGson>> listImages();
+    fun listImages(): Call<List<ImageGson>>
 }
