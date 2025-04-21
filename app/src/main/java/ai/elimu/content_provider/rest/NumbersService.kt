2e65c27b19bb5203399e88b38cf82d6083604993
tuancoltech <tuancoltech@gmail.com>
2025-04-21 12:54:45 +0700

    Convert NumbersService to Kotlin

--- app/src/main/java/ai/elimu/content_provider/rest/NumbersService.kt
+++ app/src/main/java/ai/elimu/content_provider/rest/NumbersService.kt
@@ -1,13 +1,10 @@
-package ai.elimu.content_provider.rest;
+package ai.elimu.content_provider.rest
 
-import java.util.List;
+import ai.elimu.model.v2.gson.content.NumberGson
+import retrofit2.Call
+import retrofit2.http.GET
 
-import ai.elimu.model.v2.gson.content.NumberGson;
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface NumbersService {
-
+interface NumbersService {
     @GET("content/numbers")
-    Call<List<NumberGson>> listNumbers();
+    fun listNumbers(): Call<List<NumberGson>>
 }
