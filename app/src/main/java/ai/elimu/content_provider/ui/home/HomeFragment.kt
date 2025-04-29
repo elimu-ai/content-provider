bd0145c9f3cfe8283bccfdfcaa9d14dce37b84f9
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-29 02:58:07 +0000

    Replace findViewById with viewBinding

--- app/src/main/java/ai/elimu/content_provider/ui/home/HomeFragment.kt
+++ app/src/main/java/ai/elimu/content_provider/ui/home/HomeFragment.kt
@@ -1,12 +1,11 @@
 package ai.elimu.content_provider.ui.home
 
-import ai.elimu.content_provider.R
+import ai.elimu.content_provider.databinding.FragmentHomeBinding
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +13,7 @@ import androidx.lifecycle.ViewModelProvider
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,14 +23,13 @@ class HomeFragment : Fragment() {
         Log.i(javaClass.name, "onCreateView")
 
         homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView = root.findViewById<TextView>(R.id.text_home)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
         homeViewModel.getText().observe(viewLifecycleOwner, object : Observer<String?> {
             override fun onChanged(s: String?) {
                 Log.i(javaClass.name, "onChanged")
-                textView.text = s
+                binding.textHome.text = s
             }
         })
-        return root
+        return binding.root
     }
 }
