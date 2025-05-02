611bb0f965a67256c824f4c079f0e0cbc10dffb2
tuancoltech <tuancoltech@gmail.com>
2025-05-02 15:27:21 +0700

    Convert LetterSound_SoundDao to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/dao/LetterSound_SoundDao.kt
+++ app/src/main/java/ai/elimu/content_provider/room/dao/LetterSound_SoundDao.kt
@@ -1,17 +1,15 @@
-package ai.elimu.content_provider.room.dao;
+package ai.elimu.content_provider.room.dao
 
-import androidx.room.Dao;
-import androidx.room.Insert;
-import androidx.room.Query;
-
-import ai.elimu.content_provider.room.entity.LetterSound_Sound;
+import ai.elimu.content_provider.room.entity.LetterSound_Sound
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
-public interface LetterSound_SoundDao {
-
+interface LetterSound_SoundDao {
     @Insert
-    void insert(LetterSound_Sound letterSound_Sound);
+    fun insert(letterSound_Sound: LetterSound_Sound)
 
     @Query("DELETE FROM LetterSound_Sound")
-    void deleteAll();
+    fun deleteAll()
 }
