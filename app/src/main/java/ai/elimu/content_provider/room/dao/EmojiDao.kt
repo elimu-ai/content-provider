9df88a5b531fbf9f51547115917368e9ae6e81e4
tuancoltech <tuancoltech@gmail.com>
2025-05-02 14:46:13 +0700

    Convert EmojiDao to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/dao/EmojiDao.kt
+++ app/src/main/java/ai/elimu/content_provider/room/dao/EmojiDao.kt
@@ -1,40 +1,35 @@
-package ai.elimu.content_provider.room.dao;
+package ai.elimu.content_provider.room.dao
 
-import android.database.Cursor;
-
-import androidx.room.Dao;
-import androidx.room.Insert;
-import androidx.room.Query;
-import androidx.room.Update;
-
-import java.util.List;
-
-import ai.elimu.content_provider.room.entity.Emoji;
+import ai.elimu.content_provider.room.entity.Emoji
+import android.database.Cursor
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
-public interface EmojiDao {
-
+interface EmojiDao {
     @Insert
-    void insert(Emoji emoji);
+    fun insert(emoji: Emoji)
 
     @Query("SELECT * FROM Emoji WHERE id = :id")
-    Emoji load(Long id);
+    fun load(id: Long?): Emoji?
 
     @Query("SELECT * FROM Emoji WHERE id = :id")
-    Cursor loadAsCursor(Long id);
+    fun loadAsCursor(id: Long?): Cursor
 
     @Query("SELECT * FROM Emoji")
-    List<Emoji> loadAll();
+    fun loadAll(): MutableList<Emoji>
 
     @Query("SELECT * FROM Emoji")
-    Cursor loadAllAsCursor();
+    fun loadAllAsCursor(): Cursor
 
     @Query("SELECT * FROM Emoji e WHERE e.id IN (SELECT Emoji_id FROM Emoji_Word WHERE words_id = :wordId)")
-    Cursor loadAllByWordLabelAsCursor(Long wordId);
+    fun loadAllByWordLabelAsCursor(wordId: Long?): Cursor
 
     @Update
-    void update(Emoji emoji);
+    fun update(emoji: Emoji)
 
     @Query("DELETE FROM Emoji")
-    void deleteAll();
+    fun deleteAll()
 }
