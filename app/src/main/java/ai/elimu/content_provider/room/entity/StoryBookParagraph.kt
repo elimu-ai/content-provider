7be599c0bd313b5249b68c3744597ffd6a5d5ba7
tuancoltech <tuancoltech@gmail.com>
2025-05-04 14:58:16 +0700

    Convert StoryBookParagraph entity class to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/entity/StoryBookParagraph.kt
+++ app/src/main/java/ai/elimu/content_provider/room/entity/StoryBookParagraph.kt
@@ -1,44 +1,18 @@
-package ai.elimu.content_provider.room.entity;
+package ai.elimu.content_provider.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
+import androidx.room.Entity
 
 /**
- * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model">model</a>
+ * For documentation, see [model](https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model)
  */
 @Entity
-public class StoryBookParagraph extends BaseEntity {
+class StoryBookParagraph : BaseEntity() {
+    @JvmField
+    var storyBookChapterId: Long = 0
 
-    @NonNull
-    private long storyBookChapterId;
+    @JvmField
+    var sortOrder: Int = 0
 
-    @NonNull
-    private Integer sortOrder;
-
-    @NonNull
-    private String originalText;
-
-    public long getStoryBookChapterId() {
-        return storyBookChapterId;
-    }
-
-    public void setStoryBookChapterId(long storyBookChapterId) {
-        this.storyBookChapterId = storyBookChapterId;
-    }
-
-    public Integer getSortOrder() {
-        return sortOrder;
-    }
-
-    public void setSortOrder(Integer sortOrder) {
-        this.sortOrder = sortOrder;
-    }
-
-    public String getOriginalText() {
-        return originalText;
-    }
-
-    public void setOriginalText(String originalText) {
-        this.originalText = originalText;
-    }
+    @JvmField
+    var originalText: String = ""
 }
