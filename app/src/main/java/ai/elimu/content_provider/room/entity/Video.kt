99756f10777a12c2519d6b6e22a9808fc3b9ca2f
tuancoltech <tuancoltech@gmail.com>
2025-05-04 14:27:11 +0700

    Convert Video entity class to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/entity/Video.kt
+++ app/src/main/java/ai/elimu/content_provider/room/entity/Video.kt
@@ -1,35 +1,15 @@
-package ai.elimu.content_provider.room.entity;
+package ai.elimu.content_provider.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
-
-import ai.elimu.model.v2.enums.content.VideoFormat;
+import ai.elimu.model.v2.enums.content.VideoFormat
+import androidx.room.Entity
 
 /**
- * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model">model</a>
+ * For documentation, see [model](https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model)
  */
 @Entity
-public class Video extends Content {
+class Video : Content() {
+    @JvmField
+    var title: String = ""
 
-    @NonNull
-    private String title;
-
-    @NonNull
-    private VideoFormat videoFormat;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public VideoFormat getVideoFormat() {
-        return videoFormat;
-    }
-
-    public void setVideoFormat(VideoFormat videoFormat) {
-        this.videoFormat = videoFormat;
-    }
+    lateinit var videoFormat: VideoFormat
 }
