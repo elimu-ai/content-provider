8c4268ea4a5f4dbf6c6943f021be6318bc342982
tuancoltech <tuancoltech@gmail.com>
2025-05-04 15:01:02 +0700

    Convert StoryBookChapter entity class to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/entity/StoryBookChapter.kt
+++ app/src/main/java/ai/elimu/content_provider/room/entity/StoryBookChapter.kt
@@ -1,44 +1,18 @@
-package ai.elimu.content_provider.room.entity;
+package ai.elimu.content_provider.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
+import androidx.room.Entity
 
 /**
- * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model">model</a>
+ * For documentation, see [model](https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model)
  */
 @Entity
-public class StoryBookChapter extends BaseEntity {
+class StoryBookChapter : BaseEntity() {
+    @JvmField
+    var storyBookId: Long = 0
 
-    @NonNull
-    private long storyBookId;
+    @JvmField
+    var sortOrder: Int = 0
 
-    @NonNull
-    private Integer sortOrder;
-
-    @NonNull
-    private long imageId;
-
-    public long getStoryBookId() {
-        return storyBookId;
-    }
-
-    public void setStoryBookId(long storyBookId) {
-        this.storyBookId = storyBookId;
-    }
-
-    public Integer getSortOrder() {
-        return sortOrder;
-    }
-
-    public void setSortOrder(Integer sortOrder) {
-        this.sortOrder = sortOrder;
-    }
-
-    public long getImageId() {
-        return imageId;
-    }
-
-    public void setImageId(long imageId) {
-        this.imageId = imageId;
-    }
+    @JvmField
+    var imageId: Long = 0
 }
