37ddf95e9feab72c2e8f4928d88c662975706c44
tuancoltech <tuancoltech@gmail.com>
2025-05-04 13:47:14 +0700

    Convert Image entity class to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/entity/Image.kt
+++ app/src/main/java/ai/elimu/content_provider/room/entity/Image.kt
@@ -1,35 +1,14 @@
-package ai.elimu.content_provider.room.entity;
+package ai.elimu.content_provider.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
-
-import ai.elimu.model.v2.enums.content.ImageFormat;
+import ai.elimu.model.v2.enums.content.ImageFormat
+import androidx.room.Entity
 
 /**
- * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model">model</a>
+ * For documentation, see [model](https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model)
  */
 @Entity
-public class Image extends Content {
+class Image : Content() {
+    lateinit var title: String
 
-    @NonNull
-    private String title;
-
-    @NonNull
-    private ImageFormat imageFormat;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public ImageFormat getImageFormat() {
-        return imageFormat;
-    }
-
-    public void setImageFormat(ImageFormat imageFormat) {
-        this.imageFormat = imageFormat;
-    }
+    lateinit var imageFormat: ImageFormat
 }
