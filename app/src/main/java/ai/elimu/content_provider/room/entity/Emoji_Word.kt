a226a1aa6211b22786c1082939fd5fe0c8c609d2
tuancoltech <tuancoltech@gmail.com>
2025-05-04 15:08:25 +0700

    Convert Emoji_Word entity class to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/entity/Emoji_Word.kt
+++ app/src/main/java/ai/elimu/content_provider/room/entity/Emoji_Word.kt
@@ -1,34 +1,14 @@
-package ai.elimu.content_provider.room.entity;
+package ai.elimu.content_provider.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
+import androidx.room.Entity
 
 /**
- * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model">model</a>
+ * For documentation, see [model](https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model)
  */
-@Entity(primaryKeys = {"Emoji_id", "words_id"})
-public class Emoji_Word {
+@Entity(primaryKeys = ["Emoji_id", "words_id"])
+class Emoji_Word {
+    var Emoji_id: Long = 0L
 
-    @NonNull
-    private Long Emoji_id;
-
-    @NonNull
-    private Long words_id;
-
-    @NonNull
-    public Long getEmoji_id() {
-        return Emoji_id;
-    }
-
-    public void setEmoji_id(Long emoji_id) {
-        Emoji_id = emoji_id;
-    }
-
-    public Long getWords_id() {
-        return words_id;
-    }
-
-    public void setWords_id(Long words_id) {
-        this.words_id = words_id;
-    }
+    @JvmField
+    var words_id: Long = 0L
 }
