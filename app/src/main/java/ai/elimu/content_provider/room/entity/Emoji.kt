381937ea26af9a07cfe410b2b10b42bb7f9d978d
tuancoltech <tuancoltech@gmail.com>
2025-05-04 12:31:17 +0700

    Convert Emoji entity class to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/entity/Emoji.kt
+++ app/src/main/java/ai/elimu/content_provider/room/entity/Emoji.kt
@@ -1,44 +1,15 @@
-package ai.elimu.content_provider.room.entity;
+package ai.elimu.content_provider.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
+import androidx.room.Entity
 
 /**
- * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model">model</a>
+ * For documentation, see [model](https://github.com/elimu-ai/webapp/tree/main/src/main/java/ai/elimu/model)
  */
 @Entity
-public class Emoji extends Content {
+class Emoji : Content() {
+    var glyph: String = ""
 
-    @NonNull
-    private String glyph;
+    var unicodeVersion: Double = 0.0
 
-    @NonNull
-    private Double unicodeVersion;
-
-    @NonNull
-    private Double unicodeEmojiVersion;
-
-    public String getGlyph() {
-        return glyph;
-    }
-
-    public void setGlyph(String glyph) {
-        this.glyph = glyph;
-    }
-
-    public Double getUnicodeVersion() {
-        return unicodeVersion;
-    }
-
-    public void setUnicodeVersion(Double unicodeVersion) {
-        this.unicodeVersion = unicodeVersion;
-    }
-
-    public Double getUnicodeEmojiVersion() {
-        return unicodeEmojiVersion;
-    }
-
-    public void setUnicodeEmojiVersion(Double unicodeEmojiVersion) {
-        this.unicodeEmojiVersion = unicodeEmojiVersion;
-    }
+    var unicodeEmojiVersion: Double = 0.0
 }
