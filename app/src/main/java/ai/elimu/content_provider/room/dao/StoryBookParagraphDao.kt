8b0fc7dcc2a5f2d8d729505aba7a2d288e4cbb7f
tuancoltech <tuancoltech@gmail.com>
2025-05-05 08:27:47 +0700

    Convert StoryBookParagraphDao interface to Kotlin

--- app/src/main/java/ai/elimu/content_provider/room/dao/StoryBookParagraphDao.kt
+++ app/src/main/java/ai/elimu/content_provider/room/dao/StoryBookParagraphDao.kt
@@ -1,37 +1,32 @@
-package ai.elimu.content_provider.room.dao;
+package ai.elimu.content_provider.room.dao
 
-import android.database.Cursor;
-
-import androidx.room.Dao;
-import androidx.room.Insert;
-import androidx.room.Query;
-import androidx.room.Update;
-
-import java.util.List;
-
-import ai.elimu.content_provider.room.entity.StoryBookParagraph;
+import ai.elimu.content_provider.room.entity.StoryBookParagraph
+import android.database.Cursor
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 
 @Dao
-public interface StoryBookParagraphDao {
-
+interface StoryBookParagraphDao {
     @Insert
-    void insert(StoryBookParagraph storyBookParagraph);
+    fun insert(storyBookParagraph: StoryBookParagraph)
 
     @Query("SELECT * FROM StoryBookParagraph WHERE id = :id")
-    StoryBookParagraph load(Long id);
+    fun load(id: Long?): StoryBookParagraph?
 
     @Query("SELECT * FROM StoryBookParagraph WHERE id = :id")
-    Cursor loadAsCursor(Long id);
+    fun loadAsCursor(id: Long?): Cursor?
 
     @Query("SELECT * FROM StoryBookParagraph WHERE storyBookChapterId = :storyBookChapterId")
-    List<StoryBookParagraph> loadAll(Long storyBookChapterId);
+    fun loadAll(storyBookChapterId: Long?): MutableList<StoryBookParagraph?>?
 
     @Query("SELECT * FROM StoryBookParagraph WHERE storyBookChapterId = :storyBookChapterId")
-    Cursor loadAllAsCursor(Long storyBookChapterId);
+    fun loadAllAsCursor(storyBookChapterId: Long?): Cursor
 
     @Update
-    void update(StoryBookParagraph storyBookParagraph);
+    fun update(storyBookParagraph: StoryBookParagraph)
 
     @Query("DELETE FROM StoryBookParagraph")
-    void deleteAll();
+    fun deleteAll()
 }
